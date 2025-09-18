f1089ac94b932e54a879a824019e892101380bf9
juanperez1710 <junpemarc@gmail.com>
2025-09-18 10:07:31 -0600

    Se realiza lab-14

---
+++ laboratorio-14/app.js
@@ -0,0 +1,10 @@
+function mostrarAlerta(){
+    alert("¡Es hora de una hamburguesa!");
+}
+function mensajeSecreto(){
+    console.log("¡Alguien ha hecho clic en Burguer Town!")
+}
+function cambiarColor(){
+    let texto=document.getElementById('titulo');
+    titulo.style.color="red"
+}
